bfddf48ec3a070147ceb86da00d6495033ca2c90
Denys Lygach <denisligach98@gmail.com>
2025-08-16 18:45:29 +0300

    Return starter kit routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,22 @@
 use App\Http\Controllers\PageCourseController;
 use App\Http\Controllers\PageHomeController;
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 Route::get('/', PageHomeController::class)->name('pages.home');
 
 Route::get('/courses/{course:slug}', PageCourseController::class)->name('pages.course-details');
+
+Route::view('dashboard', 'pages.dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
 
 require __DIR__.'/auth.php';
