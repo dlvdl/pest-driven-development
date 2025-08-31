e5789ce14ce31717587c8bec0268322064aea227
Denys Lygach <denisligach98@gmail.com>
2025-08-31 21:51:44 +0300

    Add tests Paddle jobs

--- tests/Feature/WebHookPaddlePurchaseTest.php
+++ tests/Feature/WebHookPaddlePurchaseTest.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\Jobs\HandlePaddlePurchaseJob;
 use Spatie\WebhookClient\Models\WebhookCall;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\post;
@@ -10,14 +11,15 @@ use function Pest\Laravel\withoutExceptionHandling;
 test('stores a paddle purchase request', function () {
     assertDatabaseCount(WebhookCall::class, 0);
 
-    list($signature, $paddleRequestData) = generatePaddleTestData();
+    [$signature, $paddleRequestData] = generatePaddleTestData();
 
     postJson(
         'webhooks',
         $paddleRequestData,
         [
             'paddle-signature' => $signature
-        ]);
+        ]
+    );
 
     assertDatabaseCount(WebhookCall::class, 1);
 });
@@ -30,3 +32,34 @@ test('does not store invalid paddle purchase request', function () {
     assertDatabaseCount(WebhookCall::class, 0);
 });
 
+test('dispatches a job for valid paddle request', function () {
+    Queue::fake();
+
+    [$signature, $paddleRequestData] = generatePaddleTestData();
+
+    postJson(
+        'webhooks',
+        $paddleRequestData,
+        [
+            'paddle-signature' => $signature
+        ]
+    );
+
+    Queue::assertPushed(HandlePaddlePurchaseJob::class);
+});
+
+test('does not dispatch job for invalid paddle request', function () {
+    Queue::fake();
+
+    [$signature, $paddleRequestData] = generatePaddleTestData();
+
+    postJson(
+        'webhooks',
+        [],
+        [
+            'paddle-signature' => $signature
+        ]
+    );
+
+    Queue::assertNothingPushed();
+});
